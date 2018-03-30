07a98c787d025644385d41efe973e452c10601cc
Pasha Pourmand <pasha.pourmand@gmail.com>
2018-03-29 19:42:51 -0700

    Initial Commit

---
+++ CustomDnd/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CustomDnd
+//
+//  Created by Pasha Pourmand on 3/29/18.
+//  Copyright © 2018 Pasha Pourmand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
